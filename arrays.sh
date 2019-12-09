0880f227175a6344bb4cf510abf5707dba6f013a
ChristopherSaid <54338175+ChristopherSaid@users.noreply.github.com>
2019-12-08 22:32:15 -0500

    Create arrays.sh

---
+++ arrays.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+arreglo[0]="dato random"
+arreglo[1]="dato conocido"
+arreglo[2]="dato erroneo"
+arreglo[3]="dato correcto"
+
+echo ${arreglo[0]} es un indefinido, a lo cual
+echo ${arreglo[1]} puede repetirse
