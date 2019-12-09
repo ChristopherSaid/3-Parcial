bcb8f6f3794eb7062cf28c8d1e76c0d79b67be9b
ChristopherSaid <54338175+ChristopherSaid@users.noreply.github.com>
2019-12-08 22:38:33 -0500

    Create variables.sh

---
+++ variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+una_variable=666
+palabra1=onomatopeya
+maistro=tuxxter
+echo "A numa mi variable es ${una_variable}"
+echo "y ahora multiples variables ya sea una ${palabra1} enseñada por ${maistro}"
